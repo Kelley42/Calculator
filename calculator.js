a69f2497b44dcfbd37cf28bfd9da051c9a604a3f
Kelley42 <kelleykristine42@yahoo.com>
2022-07-22 16:41:05 -0500

    Added querySelectors

---
+++ calculator.js
@@ -0,0 +1,25 @@
+
+
+// Query selectors
+const backspacebutton = document.querySelector("#backspace")
+const clearbutton = document.querySelector("#clear")
+const parenthesesbutton = document.querySelector("#parentheses")
+const percentbutton = document.querySelector("#percent")
+const divisionbutton = document.querySelector("#division")
+const sevenbutton = document.querySelector("#seven")
+const eightbutton = document.querySelector("#eight")
+const ninebutton = document.querySelector("#nine")
+const multiplybutton = document.querySelector("#multiply")                
+const fourbutton = document.querySelector("four")               
+const fivebutton = document.querySelector("#five") 
+const sixbutton = document.querySelector("#six")               
+const minusbutton = document.querySelector("#minus")                
+const onebutton = document.querySelector("#one") 
+const twobutton = document.querySelector("#two")   
+const threebutton = document.querySelector("#three")  
+const plusbutton = document.querySelector("#plus")          
+const posnegbutton = document.querySelector("#pos-neg") 
+const zerobutton = document.querySelector("#zero")   
+const decimalbutton = document.querySelector("#decimal")
+const equalsbutton = document.querySelector("#equals")            
+        
